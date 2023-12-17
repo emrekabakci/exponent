23a7ebd3a768b42bd5f807d8e5b5fc4f751cf284
emrekabakci <emrekabakci.2@icloud.com>
2023-12-17 23:25:20 +0300

    Comment added.

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int num,exp,total = 1;
+
+        //Scanner created.
         Scanner scanner=new Scanner(System.in);
 
+        //Data retrieved from user.
         System.out.print("Enter the number you want to calculate the exponent : ");
         num= scanner.nextInt();
 
         System.out.print("Enter the value of the exponent : ");
         exp= scanner.nextInt();
 
+        //Exponent calculated
         for (int i=1;i<=exp;i++) {
             total=total*num;
         }
+        //Result printed.
         System.out.print(exp + " to the power of "+ num + " is "+total );
     }
 }
